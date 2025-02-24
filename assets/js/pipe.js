dfe36f8f5b410297895e45871e36742fc9d30cec
Sergey Korobeynikov <korob3ynikovsergey@yandex.ru>
2025-02-24 16:13:57 +1000

    Upload Project

---
+++ assets/js/pipe.js
@@ -0,0 +1,52 @@
+import { loadImage } from "./utils.js";
+
+export class Pipe {
+    static width = 100;
+    static topPipeImg;
+    static bottomPipeImg;
+
+    width = Pipe.width;
+    spacing = 220;
+
+    static async preloadImages() {
+        Pipe.topPipeImg = new Image();
+        Pipe.bottomPipeImg = new Image();
+
+        await Promise.all([
+            loadImage(Pipe.topPipeImg, './assets/img/top-pipe.png'),
+            loadImage(Pipe.bottomPipeImg, './assets/img/bottom-pipe.png')
+        ]);
+    }
+
+    constructor(canvas) {
+        this.canvas = canvas;
+        this.ctx = canvas.getContext("2d");
+        this.canvasHeight = canvas.height;
+        this.top = this.canvasHeight / 10 + Math.random() * this.canvasHeight / 3;
+        this.bottom = this.top + this.spacing;
+        this.x = canvas.width;
+    }
+
+    draw() {
+        this.ctx.drawImage(
+            Pipe.topPipeImg,
+            this.x,
+            this.top - Pipe.topPipeImg.height,
+        );
+
+        this.ctx.drawImage(
+            Pipe.bottomPipeImg,
+            this.x,
+            this.bottom,
+        );
+    }
+
+    update(speed = 3) {
+        this.x -= speed;
+        this.draw();
+    }
+
+    isOffScreen() {
+        return this.x < -this.width;
+    }
+}
